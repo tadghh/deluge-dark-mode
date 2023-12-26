1112527c888862f1292ff19148165c88fb26e315
tadghh <thenry.he@gmail.com>
2023-12-25 22:50:13 -0600

    Added paramter

--- Set-deluge-dark-mode.ps1
+++ Set-deluge-dark-mode.ps1
@@ -1,8 +1,12 @@
 ## TODO: Add paramter to enable or disable
 ## TODO: Check deluge version number
+param(
+	[Parameter()]
+	[switch]$dt = $false
+)
+
 $settingHeader = '[Settings]'
 $settingValue = 'gtk-application-prefer-dark-theme='
-
 $exeName = "deluge.exe"
 
 # Scoop install directory
@@ -11,16 +15,12 @@ $scoopInstall = "$env:USERPROFILE\scoop\apps\deluge\current\"
 # Default install directory
 $defaultInstallDir = "C:\Program Files\Deluge\"
 $isDefault = $false
-$currentInstallPath = ""
+$currentInstallPath = $scoopInstall
 
 if ( Test-Path ($defaultInstallDir + $exeName)) {
 	$isDefault = $true
 	$currentInstallPath = $defaultInstallDir
 }
-else {
-	$currentInstallPath = $scoopInstall
-}
-
 
 
 # Checks if default exists or if scoop
@@ -34,6 +34,7 @@ if ( $isDefault -Or (Test-Path ($currentInstallPath + $exeName)) ) {
 	if (!(Test-Path ($currentInstallPath + $shareName + $gtkName + $settingsFile))) {
 		# Test for share
 		$validPaths = $false
+
 		if (Test-Path ($currentInstallPath + $shareName)) {
 			$validPaths = $true
 			# mkdir $gtkName
@@ -45,16 +46,15 @@ if ( $isDefault -Or (Test-Path ($currentInstallPath + $exeName)) ) {
 			Write-Host "Created: settings.ini"
 			New-Item -ItemType File -Path $currentSettingsPath + $settingsFile
 		}
-
 	}
 
 	if ( $validPaths ) {
-		$currentSettingsPath = $currentInstallPath + $shareName + $gtkName + $settingsFile
+		$currentSettingsPath = ($currentInstallPath + $shareName + $gtkName + $settingsFile)
 
 		$temp = $false
-
+		$settingsHeaderExists = (Get-Content -Path $currentSettingsPath | Select-String $settingHeader -Quiet)
 		# Write settings $settingHeader
-		if ($false -eq $settingsHeaderExists) {
+		if (-not $settingsHeaderExists) {
 			Write-Host "Didn't find header"
 			$settingsFileContent += ("`n" + $settingHeader)
 		}
@@ -62,38 +62,23 @@ if ( $isDefault -Or (Test-Path ($currentInstallPath + $exeName)) ) {
 		#Check if value exists already, otherwise add it
 		$darkValueLineNumber = (Select-String -Path $currentSettingsPath -Pattern $settingValue -List).LineNumber
 		if ( $null -eq $darkValueLineNumber) {
-			$settingsFileContent += ("`n" + $settingValue + "true")
+			$settingsFileContent += ("`n" + $settingValue + $dt)
 		}
 		else {
+			$settingsFileContent = Get-Content -Path $currentSettingsPath
 			# The value exists, replace the value of it
-			# System link issue, current folder and version folder
-			Write-Host "Mod"
-			$fileContent2 = (Get-Content -Path "D:\Personal\Projects\Coding\Powershell\deluge-dark-mode\settings.ini")
-			Write-Host $fileContent2.Count
+			#Update file content
+			#Write-Host $darkValueLineNumber
+			$settingsFileContent[$darkValueLineNumber - 1] = ($settingsFileContent[$darkValueLineNumber - 1] -replace '=.*$', ('=' + $dt.ToString().ToLower()))
 
-			Write-Host "Before"
-			foreach ($line in $fileContent) {
-				Write-Host $line
-			}
-
-			#Update file content, false should be param later
-			$regex = '^\s*gtk-application-prefer-dark-theme\s*=\s*(true|false)\s*$'
-			$newValue = 'gtk-application-prefer-dark-theme=false'
-
-			$null = (Get-Content $currentSettingsPath) -replace $regex, $newValue | Set-Content -Path $currentSettingsPath -Force
-			Write-Host "After"
-
-			foreach ($line in $fileContent) {
-				Write-Host $line
-			}
-			#$fileContent | Set-Content -Path $currentSettingsPath -Force
+			#$settingsFileContent[$darkValueLineNumber - 1]
+			$settingsFileContent | Set-Content -Path $currentSettingsPath -Force
 			$temp = $true
 		}
 		if ($temp -eq $false) {
 			#We are only adding the header and default dark mode value
 			$settingsFileContent | Set-Content -Path $currentSettingsPath -Force
 		}
-
 	}
 	else {
 		Write-Host "Couldn't create paths"
