6b09399f9337b340fd1ae1a291ebd7965a76cb2c
tadghh <thenry.he@gmail.com>
2023-12-26 01:59:32 -0600

    Testing and fixes

--- Set-deluge-dark-mode.ps1
+++ Set-deluge-dark-mode.ps1
@@ -1,8 +1,10 @@
 ## TODO: Check deluge version number
 param(
 	[Parameter()]
-	[switch]$dt = $false
+	[switch]$dt = $false,
+	[String]$installLocation
 )
+$ErrorActionPreference = "Stop"
 
 $settingHeader = '[Settings]'
 $settingValue = 'gtk-application-prefer-dark-theme='
@@ -10,79 +12,115 @@ $exeName = "deluge.exe"
 
 # Scoop install directory
 $scoopInstall = "$env:USERPROFILE\scoop\apps\deluge\current\"
-
-# Default install directory
 $defaultInstallDir = "C:\Program Files\Deluge\"
-$isDefault = $false
-$currentInstallPath = $scoopInstall
+$currentInstallPath = $null
+$validInstallPath = $false
 
-if ( Test-Path ($defaultInstallDir + $exeName)) {
-	$isDefault = $true
+# Check install locations
+if ($installLocation) {
+	if ( $installLocation -contains ".exe") {
+		$installLocation = $installLocation.Substring(0, $installLocation.LastIndexOf('\'))
+	}
+
+	if (Test-Path ($installLocation + $exeName)) {
+		$validInstallPath = $true
+
+		$currentInstallPath = $installLocation
+	}
+	else {
+		Write-Host "Could not find deluge.exe in $installLocation"
+	}
+}
+elseif (Test-Path ($defaultInstallDir + $exeName)) {
+	Write-Host "Found Deluge in $defaultInstallDir"
+	$validInstallPath = $true
 	$currentInstallPath = $defaultInstallDir
+}
+elseif ( Test-Path ("$env:USERPROFILE\scoop")) {
+	#Checking Scoop
+	Write-Host "Scoop folder exists, checking deluge"
+
+	#Regex is expensive
+	#$scoopDelugePresent = ( scoop list | Format-List -Property "Name" | Out-String | Select-String -pattern "deluge")
+	$scoopDelugePresent = scoop prefix "deluge"
+	if ($scoopDelugePresent -And (Test-Path ($scoopInstall + $exeName))) {
+		Write-Host "Found Scoop, Deluge install location"
+		$validInstallPath = $true
+
+		$currentInstallPath = $scoopInstall
+	}
+	else {
+		Write-Host "Deluge not found `n $scoopDelugePresent"
+	}
+}
+else {
+	Write-Host "No install path found"
 }
 
 
 # Checks if default exists or if scoop.
-if ( $isDefault -Or (Test-Path ($currentInstallPath + $exeName)) ) {
+if ($validInstallPath) {
 	$shareName = "\share"
 	$gtkName = "\gtk-3.0"
 	$settingsFile = "\settings.ini"
 	$validPaths = $true
+	try {
+		# Hopeful Path: Test for share and GTK.
+		if (-not (Test-Path ($currentInstallPath + $shareName + $gtkName + $settingsFile))) {
+			# Test for share
+			$validPaths = $false
 
-	# Hopeful Path: Test for share and GTK.
-	if (!(Test-Path ($currentInstallPath + $shareName + $gtkName + $settingsFile))) {
-		# Test for share
-		$validPaths = $false
+			if (Test-Path ($currentInstallPath + $shareName)) {
+				$validPaths = $true
+				# mkdir $gtkName
+				$currentSettingsPath = ($currentInstallPath + $shareName + $gtkName)
+				New-Item -ItemType Directory -Path $currentSettingsPath
+				Write-Host "Created: GTK Folder"
 
-		if (Test-Path ($currentInstallPath + $shareName)) {
-			$validPaths = $true
-			# mkdir $gtkName
-			$currentSettingsPath = $currentInstallPath + $shareName + $gtkName
-			Write-Host "Created: GTK Folder"
-			New-Item -ItemType Directory -Path $currentSettingsPath
+				# make settings file
+				New-Item -ItemType File -Path ($currentSettingsPath + $settingsFile)
+				Write-Host "Created: settings.ini"
 
-			# make settings file
-			Write-Host "Created: settings.ini"
-			New-Item -ItemType File -Path $currentSettingsPath + $settingsFile
+			}
+		}
+
+		if ( $validPaths ) {
+			$currentSettingsPath = ($currentInstallPath + $shareName + $gtkName + $settingsFile)
+			$settingsFileContent = Get-Content -Path $currentSettingsPath
+			$changes = $false
+
+			# Missing header section in settings file.
+			if (-not ($settingsFileContent -contains $settingHeader)) {
+				$changes = $true
+				$settingsFileContent += "`n$settingHeader"
+			}
+
+			$passedModeString = $dt.ToString().ToLower()
+			$darkValueLineNumber = (Select-String -Path $currentSettingsPath -Pattern $settingValue).LineNumber
+
+			# Missing dark mode key, add it.
+			if ($null -eq $darkValueLineNumber) {
+				$changes = $true
+				$settingsFileContent += "`n$settingValue$passedModeString"
+			}
+			else {
+				$changes = $true
+				$valueLineNumber = $darkValueLineNumber - 1
+
+				# The setting key exists, replace the value of it
+				$settingsFileContent[$valueLineNumber] = $settingsFileContent[$valueLineNumber] -replace '=.*$', "=$passedModeString"
+			}
+			if ($changes) {
+				#We are only adding the header and default dark mode value
+				$settingsFileContent | Set-Content -Path $currentSettingsPath -Force -ErrorAction Stop
+			}
 		}
 	}
-
-	if ( $validPaths ) {
-		$currentSettingsPath = ($currentInstallPath + $shareName + $gtkName + $settingsFile)
-		$settingsFileContent = Get-Content -Path $currentSettingsPath
-		$changes = $false
-
-		# Missing header section in settings file.
-		if (-not ($settingsFileContent -contains $settingHeader)) {
-			$changes = $true
-			$settingsFileContent += "`n$settingHeader"
-		}
-
-		$passedModeString = $dt.ToString().ToLower()
-		$darkValueLineNumber = (Select-String -Path $currentSettingsPath -Pattern $settingValue).LineNumber
-
-		# Missing dark mode key, add it.
-		if ($null -eq $darkValueLineNumber) {
-			$changes = $true
-			$settingsFileContent += "`n$settingValue$passedModeString"
-		}
-		else {
-			$changes = $true
-			$valueLineNumber = $darkValueLineNumber - 1
-
-			# The setting key exists, replace the value of it
-			$settingsFileContent[$valueLineNumber] = $settingsFileContent[$valueLineNumber] -replace '=.*$', "=$passedModeString"
-		}
-		if ($changes) {
-			#We are only adding the header and default dark mode value
-			$settingsFileContent | Set-Content -Path $currentSettingsPath -Force
-		}
-	}
-	else {
-		Write-Host "Couldn't create paths"
+	catch {
+		Write-Error "An error occured, try running this script as administrator: $_"
 	}
 }
 else {
-	Write-Host "Could not find deluge install."
+	Write-Error "Could not find deluge install."
 }
 # Sad Path
